ce96bb5668f7fbd5d55894d2902621ca699f6f42
paulkimtest <48825944+paulkim1997@users.noreply.github.com>
2021-05-26 10:49:10 +0900

    About Page

--- src/pages/PageAbout.vue
+++ src/pages/PageAbout.vue
@@ -1,6 +1,16 @@
 <template>
-  <q-page>
-    <h5>About</h5>
+  <q-page class="q-pa-lg">
+    <h4 class="q-mt-none q-mb-md text-weight-bold">About Qwitter</h4>
+    <div class="text-body1">
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      Maiores perspiciatis, iusto rerum aspernatur obcaecati ducimus
+      nostrum corporis! Enim aliquam ex doloribus ullam hic cumque,
+      culpa sint minima numquam, excepturi asperiores.</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      Maiores perspiciatis, iusto rerum aspernatur obcaecati ducimus
+      nostrum corporis! Enim aliquam ex doloribus ullam hic cumque,
+      culpa sint minima numquam, excepturi asperiores.</p>
+    </div>
   </q-page>
 </template>
 

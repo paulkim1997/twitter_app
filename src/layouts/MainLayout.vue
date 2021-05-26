5558a83648792a3446c8ccbd64d3fb89431ea296
paulkimtest <48825944+paulkim1997@users.noreply.github.com>
2021-05-26 10:44:55 +0900

    Routing and Pages Setting

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -6,7 +6,7 @@
         <q-btn dense flat round icon="menu" @click="left = !left" />
 
         <q-toolbar-title class="text-weight-bold">
-          <span class="gt-sm">Qwitter</span>
+          <span class="gt-sm">{{$route.name}}</span>
           <q-icon
             class="header-icon q-pa-md lt-md"
             name="fas fa-dove"
@@ -35,6 +35,7 @@
         to="/"
         clickable
         v-ripple
+        exact
       >
         <q-item-section avatar>
           <q-icon name="home" size="md"/>
@@ -44,11 +45,12 @@
           class="text-h6 text-weight-bold">
           Home
         </q-item-section>
-      </q-item>
+      </q-item> 
       <q-item
         to="/about"
         clickable
         v-ripple
+        exact
       >
         <q-item-section avatar>
           <q-icon name="help" size="md"/>

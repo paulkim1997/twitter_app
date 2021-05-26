5233b71328120c9178a474f06fef19070ebb552c
paulkimtest <48825944+paulkim1997@users.noreply.github.com>
2021-05-26 10:19:55 +0900

    Right Drawer

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -57,7 +57,49 @@
     </q-drawer>
 
     <q-drawer show-if-above v-model="right" side="right" bordered>
-      <!-- drawer content -->
+      <q-input
+        placeholder="Search Qwitter"
+        dense
+        class="q-ma-md"
+        outlined
+        rounded
+        >
+        <template v-slot:prepend>
+          <q-icon name="search" />
+        </template>
+      </q-input>
+      <q-list padding separator>
+      <q-item class="q-pa-md">
+        <q-item-section>
+          <q-item-label overline class="text-gray">Education</q-item-label>
+          <q-item-label class="text-weight-bold">Something Amazing Happened!</q-item-label>
+          <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+        </q-item-section>
+        <q-item-section side top>
+          <q-item-label caption>5 min ago</q-item-label>
+        </q-item-section>
+      </q-item>
+      <q-item class="q-pa-md">
+        <q-item-section>
+          <q-item-label overline class="text-gray">Education</q-item-label>
+          <q-item-label class="text-weight-bold">Something Amazing Happened!</q-item-label>
+          <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+        </q-item-section>
+        <q-item-section side top>
+          <q-item-label caption>5 min ago</q-item-label>
+        </q-item-section>
+      </q-item>
+      <q-item class="q-pa-md">
+        <q-item-section>
+          <q-item-label overline class="text-gray">Education</q-item-label>
+          <q-item-label class="text-weight-bold">Something Amazing Happened!</q-item-label>
+          <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+        </q-item-section>
+        <q-item-section side top>
+          <q-item-label caption>5 min ago</q-item-label>
+        </q-item-section>
+      </q-item>
+      </q-list>
     </q-drawer>
 
     <q-page-container>
